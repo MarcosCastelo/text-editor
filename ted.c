1e9dd0565282bc9127124853755372c8319a3b96
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-21 16:08:40 -0300

    Printing the keys pressed

--- ted.c
+++ ted.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
@@ -5,12 +7,19 @@
 struct termios orig_termios;
 
 void enableRawMode();
+void disableRawMode();
 
 int main() {
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
     return 0;
 }
 
