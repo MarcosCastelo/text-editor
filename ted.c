8169abdf3cfa2ce875228526e7525faa497ae86d
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-22 18:27:37 -0300

    Refactor keyboard input

--- ted.c
+++ ted.c
@@ -18,19 +18,14 @@ struct termios orig_termios;
 void enableRawMode();
 void disableRawMode();
 void die(const char *s);
+char editorReadKey();
+void editorProcessKeypress();
 
 int main() {
     enableRawMode();
     
     while(1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else { 
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == CTRL_KEY('q')) break;
+        editorProcessKeypress();
     }
 
     return 0;
@@ -62,4 +57,23 @@ void enableRawMode() {
 void die( const char *s) {
     perror(s);
     exit(1);
+}
+
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)  {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+void editorProcessKeypress() {
+    char c = editorReadKey();
+
+    switch (c) {
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
 }
