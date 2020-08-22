46ba7ce99477d49255cb9396a8f607435b7b5ce8
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-22 20:25:55 -0300

    Creating new type abuf to reduce the number of 'write' calls

--- ted.c
+++ ted.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <termios.h>
@@ -20,6 +21,29 @@ struct editorConfig {
 
 struct editorConfig E;
 
+
+/** append buffer **/
+
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend( struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
 /** init **/
 
 void enableRawMode();
@@ -30,6 +54,7 @@ void editorProcessKeypress();
 void editorRefreshScreen();
 int getWindowSize( int *rows, int *cols);
 void initEditor();
+void editorDrawRows(struct abuf *ab);
 
 
 int main() {
@@ -121,7 +146,6 @@ char editorReadKey() {
     return c;
 }
 
-
 /** input **/
 
 void editorProcessKeypress() {
@@ -138,22 +162,26 @@ void editorProcessKeypress() {
 
 
 /** output **/
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~", 1);
+        abAppend(ab, "~", 1);
 
         if (y < E.screenrows - 1) {
-            write(STDOUT_FILENO, "\r\n", 2);
+            abAppend(ab, "\r\n", 2);
         }
     }
 }
 
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H", 3);
 
-    editorDrawRows();
+    editorDrawRows(&ab);
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[H", 3);
+    
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
