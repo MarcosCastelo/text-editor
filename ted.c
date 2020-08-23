65b7406aed4b26a3195a034e8c26b19df130b81e
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-22 23:16:55 -0300

    Testing input row text

--- ted.c
+++ ted.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <termios.h>
 
@@ -45,6 +46,18 @@ struct editorConfig {
 
 struct editorConfig E;
 
+/** file i/o **/
+
+void editorOpen() {
+    char *line = "Hello, world!";
+    ssize_t linelen = 13;
+
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+}
 
 /** append buffer **/
 
@@ -83,6 +96,7 @@ void editorDrawRows(struct abuf *ab);
 int main() {
     enableRawMode();
     initEditor();
+    editorOpen();
     
     while(1) {
         editorRefreshScreen();
@@ -282,20 +296,26 @@ void editorProcessKeypress() {
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        if (y == E.screenrows / 3) {
-            char welcome[80];
-            int welcomelen = snprintf(welcome, sizeof(welcome),
-                "TED editor -- version %s", TED_VERSION);
-            if (welcomelen > E.screencols) welcomelen = E.screencols;
-            int padding = (E.screencols - welcomelen) / 2;
-            if (padding) {
-                abAppend(ab, "~", 1);
-                padding--;
-            }
-            while (padding--) abAppend(ab, " ", 1);
-            abAppend(ab, welcome, welcomelen);
+        if (y >= E.numrows) {
+          if (y == E.screenrows / 3) {
+              char welcome[80];
+              int welcomelen = snprintf(welcome, sizeof(welcome),
+                  "TED editor -- version %s", TED_VERSION);
+              if (welcomelen > E.screencols) welcomelen = E.screencols;
+              int padding = (E.screencols - welcomelen) / 2;
+              if (padding) {
+                  abAppend(ab, "~", 1);
+                  padding--;
+              }
+              while (padding--) abAppend(ab, " ", 1);
+              abAppend(ab, welcome, welcomelen);
+          } else {
+              abAppend(ab, "~", 1);
+          }
         } else {
-            abAppend(ab, "~", 1);
+          int len = E.row.size;
+          if (len > E.screencols) len = E.screencols;
+          abAppend(ab, E.row.chars, len);
         }
         
         abAppend(ab, "\x1b[K", 3);
